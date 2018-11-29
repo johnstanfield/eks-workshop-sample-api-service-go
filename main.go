abdd63ca9881e014a3ef869a060d5f0afed5ab25
johnstanfield <stanfieldjt@gmail.com>
2018-11-29 16:07:16 -0500

    said hello differently

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello re:Invent 2018"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
