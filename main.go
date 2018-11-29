35d16997980cf31c44e86740f3c4a941716175e2
johnstanfield <stanfieldjt@gmail.com>
2018-11-29 16:16:27 -0500

    another change

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello re:Invent 2018"}
+		res := &response{Message: "Hello re:Invent 2018 y'all"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
